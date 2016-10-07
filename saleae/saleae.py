5b06ffbad51d3fa0a3bcf843b1033f60a0f33610
Tris-ares <Tris-ares@users.noreply.github.com>
2016-10-07 15:39:21 +0100

    get_connected_devices: Sturdier handling response
    Response to command 'GET_CONNECTED_DEVICES' can vary from '' to 'True\n' or a previous command response. This is an improved and sturdier version of commit 288d8ef676e41551d9e18da7a85281ac067c4651

--- saleae/saleae.py
+++ saleae/saleae.py
@@ -401,7 +401,7 @@ class Saleae():
 		[<saleae.ConnectedDevice #1 LOGIC_4_DEVICE Logic 4 (...) **ACTIVE**>, <saleae.ConnectedDevice #2 LOGIC_8_DEVICE Logic 8 (...)>, <saleae.ConnectedDevice #3 LOGIC_PRO_8_DEVICE Logic Pro 8 (...)>, <saleae.ConnectedDevice #4 LOGIC_PRO_16_DEVICE Logic Pro 16 (...)>]
 		'''
 		devices = self._cmd('GET_CONNECTED_DEVICES')
-		while ('TRUE' == devices):
+		while ('' == devices or not devices[0].isdigit()):
 			time.sleep(0.1)
 			devices = self._cmd('GET_CONNECTED_DEVICES')
 
