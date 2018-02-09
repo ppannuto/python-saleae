3662cf91d7c088741ad3dc71f5fbbf494a73797f
Pat Pannuto <pat.pannuto@gmail.com>
2018-02-08 16:53:58 -0800

    add py36 to testing/supported suite

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
 			"Programming Language :: Python :: 3.3",
 			"Programming Language :: Python :: 3.4",
 			"Programming Language :: Python :: 3.5",
+			"Programming Language :: Python :: 3.6",
 			"Programming Language :: Python :: Implementation :: CPython",
 			"License :: OSI Approved :: MIT License",
 			"Operating System :: OS Independent",
